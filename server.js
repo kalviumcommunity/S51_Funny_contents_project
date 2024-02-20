d59ed77453fc40ea09eca682681c12f91dfed8a2
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-02-20 12:16:56 +0530

    error handling case added

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('', (req, res)=>{
     res.send("Hi this my first page...");
 });
 
+app.use((req, res)=>{
+    res.status(404).send("404 Not found...")
+})
+
 app.listen(port, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
   });
